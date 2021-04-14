7fc7e7f248825f6bfad165ba2f77a48247dc72d6
ananthujp <ananthujp@gmail.com>
2021-04-14 20:08:25 +0530

    1 file modified, 1 added, 1 deleted

--- src/elements/Login.js
+++ src/elements/Login.js
@@ -1,8 +1,15 @@
 import { Button } from "@material-ui/core";
 import React from "react";
+import { auth, provider } from "./firebase";
 function Login() {
   const signIn = () => {
     // sign ...
+    auth
+      .signInWithPopup(provider)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(error => alert(error.message));
   };
   return (
     <Button type="submit" onClick={signIn}>
